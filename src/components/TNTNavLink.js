5b58764d087877b8a05dcd6b001423f2b772d6b2
Sam Morton <sammorton44@gmail.com>
2021-02-09 14:27:02 -0500

    first commit

---
+++ src/components/TNTNavLink.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TNTNavLink = (props) => {
+  return (
+    <div className="tnt-navlink">
+      {props.navName}
+    </div>
+  );
+};
+
+export default TNTNavLink;
